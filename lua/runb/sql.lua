6500954b159919e441a4cbb1d65e57dec3fe9041
eumis <eumisdev@proton.me>
2025-09-09 09:26:02 +0200

    updated sql run

--- lua/runb/sql.lua
+++ lua/runb/sql.lua
@@ -2,14 +2,21 @@ local generic = require "runb.generic"
 
 local M = {
     command = "usql",
-    connection = ""
+    connection = nil
 }
 
 ---@param args string | string[]
 ---@param callback? fun(result: any)
 ---@return Job
 function M.run(args, callback)
-    if type(args) == "string" then args = { args } end
+    if type(args) == "string" then
+        args = { args }
+    elseif args == nil then
+        args = {}
+    end
+    if M.connection ~= nil then
+        table.insert(args, 1, M.connection)
+    end
     return generic.run(M.command, { args = args }, callback)
 end
 
